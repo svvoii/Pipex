097e3087f047c231615c15ce0f9b68e5d001e264
Sergiu BOCANCIA <sbocanci@e2r4p2.clusters.42paris.fr>
2023-03-07 17:10:25 +0100

    first commit..

---
+++ _pipex_/sources/pipex.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbocanci <sbocanci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 16:26:16 by sbocanci          #+#    #+#             */
+/*   Updated: 2023/03/07 17:06:52 by sbocanci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+int	main(int ac, char **av, char **envp)
+{
+	int		fd[2];
+	pid_t	pid_1;
+
+	if (ac == 5)
+	{
+		if (pipe(fd) == -1)
+			error();
+		pid_1 = fork();
+		if (pid_1 == -1)
+			error();
+		if (pid_1 == 0)
+			child_process(av, envp, fd);
+		waitpid(pid_1, NULL, 0);
+		parent_process(av, envp, fd);
+	}
+	else
+	{
+		ft_putstr_fd("Err: Bad args\n", 2);
+		ft_putstr_fd("Usage: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 1);
+	}
+	return (0);
+}
+
+void	child_process(char **av, char **envp, int fd)
+{
+	int	filein;
+
+	filein = open(av[1], O_RDONLY, 0777);
+	if (filein == -1)
+		error();
+	dup2(fd[1], SDTOUT_FILENO);
+	dup2(filein, SDTIN_FILENO);
+	close(fd[0]);
+	execute(av[2], envp);
+}
+
+void	parent_process(char **av, char **envp, int fd)
+{
+	int	fileout;
+
+	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fileout == -1)
+		error();
+	dup2(fd[0], SDTIN_FILENO);
+	dup2(fileout, SDTOUT_FILENO);
+	close(fd[1]);
+	execute(argv[3], envp);
+}
+
+void	execute(char *av, char **envp)
+{
+	char	**cmd;
+	char	*path;
+	int		i;
+
+	cmd = ft_split(av, ' ');
+	path = find_path(cmd[0], envp);
+	i = -1;
+	if (!path)
+	{
+		while (cmd[++i])
+			free(cmd[i]);
+		free(cmd);
+		error();
+	}
+	if (execve(path, cmd, envp) == -1)
+		error();
+}
+
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	char	*part_path;
+	int		i;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = -1;
+	while (paths[++i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
+}
+
+void	error()
+{
+	perror("\033[31mError")
+	exit(EXIT_FAILURE);
+}
